ec750ecde76ed777a7796f5bb380de2973a7879a
M-Bahy <mohamedayman2942002@gmail.com>
2024-12-03 20:40:18 +0200

    idle when close

--- Assets/Scripts/s_m.cs
+++ Assets/Scripts/s_m.cs
@@ -7,15 +7,28 @@ public class s_m : MonoBehaviour
 {
     NavMeshAgent agent;
     public static GameObject player;
+
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(player.transform.position);
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
     }
+
+    
 }
