9d99195b6349b3da33850d90c76ff8221351b6ab
M-Bahy <mohamedayman2942002@gmail.com>
2024-12-03 15:58:46 +0200

    spanning but under the boss and there is an error

--- Assets/Scripts/Boss_phase1_script.cs
+++ Assets/Scripts/Boss_phase1_script.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Boss_phase1_script : MonoBehaviour
 {
-    public static bool fightStarted = false;
+    public static bool fightStarted = true;
     public GameObject player;
+    public GameObject minions;
     Animator anim;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,37 @@ public class Boss_phase1_script : MonoBehaviour
     {
         // look at player
         transform.LookAt(player.transform);
-       // anim.Play("Dive bommb");
+
+        if (fightStarted){
+
+            if (canISummon())
+            {
+                summon();
+            }
+            else
+            {
+                anim.Play("Dive bommb");
+            }
+
+        }
+
+    }
+
+    bool canISummon()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Summoned_Minions");
+        return enemies.Length == 0;
+    }
+
+    public void summon()
+    {
+        anim.Play("Summoning");
+
+        int numberOfMinions = Random.Range(1, 4);
+        for (int i = 0; i < numberOfMinions; i++)
+        {
+            Vector3 pos = new Vector3(transform.position.x + Random.Range(-5, 5), transform.position.y, transform.position.z + Random.Range(-5, 5));
+            Instantiate(minions, pos, Quaternion.identity);
+        }
     }
 }
