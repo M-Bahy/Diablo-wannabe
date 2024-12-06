1e2879bf9d7908befd4262463b1db8f1cadb4a81
M-Bahy <mohamedayman2942002@gmail.com>
2024-12-06 22:00:27 +0200

    fixed healing portion position

--- Assets/Scripts/Healing_Script.cs
+++ Assets/Scripts/Healing_Script.cs
@@ -6,9 +6,9 @@ public class Healing_Script : MonoBehaviour
 {
     public GameObject healingPotionPrefab;
 
-    GameObject [] avilaibleHealingPotions;
+    List<GameObject> availableHealingPotions = new List<GameObject>();
 
-    GameObject [] collectedHealingPotions;
+    List<GameObject> collectedHealingPotions = new List<GameObject>();
 
     int numberOfHealingPotions = 10;
 
@@ -16,12 +16,37 @@ public class Healing_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     spawnHealingPotions();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void spawnHealingPotions()
+    {
+
+         // up (Z) :
+        // -80 , -20
+      // -52 , 52      --------> X
+        // down (z) :
+        // 30 , 102
+          // -30 , 30      --------> X
+      
+
+        for (int i = 0; i < numberOfHealingPotions; i++)
+        {
+           // pick up or down
+            bool up = Random.Range(0, 2) == 0;
+            // pick a random x value
+            float x =  up ? Random.Range(-52, 52) : Random.Range(-30, 30);
+            // pick a random z value
+            float z = up ? Random.Range(-80, -20) : Random.Range(30, 102);
+
+            float y = gameObject.transform.position.y;
+            availableHealingPotions.Add(Instantiate(healingPotionPrefab, new Vector3(x, y, z), Quaternion.identity));
+        }
     }
 }
