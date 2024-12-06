60ac5fdc8a0f3019fb2453ff84e765d10feccf95
n-alansary <nouralansary24@gmail.com>
2024-12-06 17:27:35 +0200

    added portal and teleport circle

--- Assets/Scripts/PlayerMechanics.cs
+++ Assets/Scripts/PlayerMechanics.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -24,7 +25,7 @@ public class PlayerMechanics : MonoBehaviour
     int requiredExp = 100;
     int playerMaxHealth = 100;
     public int playerCurrenttHealth = 90;
-    int numberOfHealingPortions = 3;
+    int numberOfHealingPortions = 0;
     int abilityPoints = 0;
     int numberOfFragments = 0;
     private Animator animator;
@@ -40,6 +41,13 @@ public class PlayerMechanics : MonoBehaviour
     public Slider expSlider ; 
 
     public TMP_Text levelText ;
+
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public GameObject portal;
+    public GameObject telepoertCircle;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -98,6 +106,8 @@ public class PlayerMechanics : MonoBehaviour
         
 
         updateHUDUI();
+
+        OpenPortal();
 
         // if(playerCurrenttHealth == 0 ){
         //     animator.Play("dead");
@@ -331,8 +341,22 @@ public class PlayerMechanics : MonoBehaviour
     {
         numberOfFragments++;
         Destroy(collision.gameObject);
+        OpenPortal();
+    }
+
+    if (collision.gameObject.tag == "portal")
+    {
+        SceneManager.LoadScene("Level2_scene");
     }
 }
 
-
+    private void OpenPortal()
+    {
+        if(numberOfFragments >= 3){
+            leftDoor.SetActive(false);
+            rightDoor.SetActive(false);
+            portal.SetActive(true);
+            telepoertCircle.SetActive(true);
+        }
+    }
 }
