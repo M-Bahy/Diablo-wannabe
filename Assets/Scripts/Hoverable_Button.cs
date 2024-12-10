37d5d3258303744acc0bb82cb089631c508c2a42
M-Bahy <mohamedayman2942002@gmail.com>
2024-12-10 20:01:43 +0200

    animation continuous glitch

--- Assets/Scripts/Hoverable_Button.cs
+++ Assets/Scripts/Hoverable_Button.cs
@@ -1,25 +1,55 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Hoverable_Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Sprite defaultSprite;
     public Sprite hoverSprite;
     private Image buttonImage;
+    private CanvasGroup canvasGroup;
+    public float fadeDuration = 0.5f;
 
     private void Awake()
     {
         buttonImage = GetComponent<Image>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        buttonImage.sprite = hoverSprite;
+        StopAllCoroutines();
+        StartCoroutine(FadeToSprite(hoverSprite));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        buttonImage.sprite = defaultSprite;
+        StopAllCoroutines();
+        StartCoroutine(FadeToSprite(defaultSprite));
+    }
+
+    private IEnumerator FadeToSprite(Sprite newSprite)
+    {
+        // Fade out
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            canvasGroup.alpha = 1 - t / fadeDuration;
+            yield return null;
+        }
+        canvasGroup.alpha = 0;
+        buttonImage.sprite = newSprite;
+
+        // Fade in
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            canvasGroup.alpha = t / fadeDuration;
+            yield return null;
+        }
+        canvasGroup.alpha = 1;
     }
 }
