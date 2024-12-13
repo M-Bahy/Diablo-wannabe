fe4f62b0466c659372ec562141183ea1af53c659
Omar Shaaban <ofahim2002@gmail.com>
2024-12-13 20:33:59 +0200

    Updated Axe Script to fix axe skill issue
    axe skill issue

--- Assets/Scripts/Axe_Script.cs
+++ Assets/Scripts/Axe_Script.cs
@@ -89,5 +89,90 @@ public class Axe_Script : MonoBehaviour
 
         }
 
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+
+
+
+        if (PlayerMechanics.barAttacking && other.CompareTag("Summoned_Minions"))
+        {
+
+            GameObject minion = other.gameObject;
+            if (minion == PlayerMechanics.minion)
+            {
+                minion.GetComponent<Minion_Logic>().TakeDamage(5);
+            }
+            // Debug.Log("Minion Hit");
+            PlayerMechanics.minion = null;
+        }
+        if (PlayerMechanics.circleAttacking && other.CompareTag("Summoned_Minions"))
+        {
+
+            if (affectedMinions.Contains(other.gameObject))
+            {
+                return;
+            }
+            GameObject minion = other.gameObject;
+            minion.GetComponent<Minion_Logic>().TakeDamage(5);
+            affectedMinions.Add(minion);
+
+            // Debug.Log("Minion Hit");
+
+        }
+        if (PlayerMechanics.barAttacking && other.CompareTag("Minion"))
+        {
+
+            GameObject minion = other.gameObject;
+            if (minion == PlayerMechanics.minion)
+            {
+                minion.GetComponent<Minion_Logic>().TakeDamage(5);
+            }
+            // Debug.Log("Minion Hit");
+            PlayerMechanics.minion = null;
+        }
+        if (PlayerMechanics.barAttacking && other.CompareTag("Demon"))
+        {
+
+            GameObject minion = other.gameObject;
+            if (minion == PlayerMechanics.minion)
+            {
+                minion.GetComponent<DemonLogic>().damageDemon(5);
+            }
+            // Debug.Log("Minion Hit");
+            PlayerMechanics.minion = null;
+        }
+
+
+        if (PlayerMechanics.circleAttacking && other.CompareTag("Minion"))
+        {
+
+            if (affectedMinions.Contains(other.gameObject))
+            {
+                return;
+            }
+            GameObject minion = other.gameObject;
+            minion.GetComponent<Minion_Logic>().TakeDamage(5);
+            affectedMinions.Add(minion);
+
+            // Debug.Log("Minion Hit");
+
+        }
+        if (PlayerMechanics.circleAttacking && (other.CompareTag("Demon")))
+        {
+
+            if (affectedMinions.Contains(other.gameObject))
+            {
+                return;
+            }
+            GameObject minion = other.gameObject;
+            minion.GetComponent<DemonLogic>().damageDemon(5);
+            affectedMinions.Add(minion);
+
+            // Debug.Log("Minion Hit");
+
+        }
+
     }
 }
